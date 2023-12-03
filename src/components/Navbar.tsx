8ddd83624997b267ef8255216bcbe4fa563b7db8
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-12-03 16:53:42 +0000

    Animating the bird, plane & sky

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -6,7 +6,7 @@ const Navbar = () => {
     <header className="header">
       <NavLink
         to="/"
-        className="w-10 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
+        className="w-20 h-10 rounded-lg bg-white items-center justify-center flex font-bold shadow-md"
       >
         <p className="blue-gradient_text">AMGE</p>
       </NavLink>
