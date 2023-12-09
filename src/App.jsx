8e73fa26a801018c09440392658bac2482961d5a
Aindriú Mac Giolla Eoin <aindriu80@gmail.com>
2023-12-09 15:24:06 +0000

    Completing the about page

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
-import { Route, BrowserRouter as Router, Routes } from 'react-router-dom'
-import Navbar from './components/Navbar.jsx'
-import Contact from './pages/Contact.jsx'
-import Projects from './pages/Projects.jsx'
-import Home from './pages/Home.jsx'
-import About from './pages/About.jsx'
+import { Route, BrowserRouter as Router, Routes } from "react-router-dom";
+import Navbar from "./components/Navbar.jsx";
+import Contact from "./pages/Contact.jsx";
+import Projects from "./pages/Projects.jsx";
+import Home from "./pages/Home.jsx";
+import About from "./pages/About.jsx";
 
 const App = () => {
   return (
-    <main className="bg-slate-300/20">
+    <main className="bg-slate-300/20 h-[100vh]">
       <Router>
         <Navbar />
         <Routes>
@@ -18,7 +18,7 @@ const App = () => {
         </Routes>
       </Router>
     </main>
-  )
-}
+  );
+};
 
-export default App
+export default App;
